beadd605c11271980138d49662f7dc27d0462592
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2018-11-23 13:37:34 +0200

    Updates

---
+++ precache-manifest.cf08ac27b289856dafb6d5b3832071f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5723f4024f101790191",
+    "url": "/react-ttt/static/js/runtime~main.b5723f40.js"
+  },
+  {
+    "revision": "0527d100c6ec11b75ec2",
+    "url": "/react-ttt/static/js/main.0527d100.chunk.js"
+  },
+  {
+    "revision": "cf251cb8abea80261373",
+    "url": "/react-ttt/static/js/1.cf251cb8.chunk.js"
+  },
+  {
+    "revision": "0527d100c6ec11b75ec2",
+    "url": "/react-ttt/static/css/main.fe7e6bbf.chunk.css"
+  },
+  {
+    "revision": "e03a991b29dd4a9844d55dac6610de48",
+    "url": "/react-ttt/index.html"
+  }
+];
